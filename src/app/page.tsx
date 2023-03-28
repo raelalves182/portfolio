d7eb1cba1110fad11d5afbb88534873d4f94021b
raelalves182 <ysrael.2009@hotmail.com>
2023-03-27 23:13:48 -0300

    feat: menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main>
         <section className="bg-hero-pattern bg-no-repeat bg-cover bg-center h-screen">
           <div className="container max-w-6xl px-6 m-auto h-full flex justify-center">
-            <div className="max-w-5xl m-auto p-16 rounded-[32px] bg-black bg-opacity-75">
+            <div className="max-w-5xl m-auto p-6 sm:p-16 rounded-[32px] bg-black bg-opacity-75">
               <h1 className="text-6xl text-[#DDDADC] font-bold">Hi, I&apos;m Israel Alves.</h1>
 
               <p className="text-2xl text-[#DDDADC] my-8">Dev In Training | Front-end Developer</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,56 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { GithubLogo, InstagramLogo, LinkedinLogo, TwitterLogo } from 'phosphor-react';
+import { GithubLogo, InstagramLogo, LinkedinLogo, List, TwitterLogo } from 'phosphor-react';
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header className="flex items-center h-auto w-full bg-[#090A0B]">
       <div className="container max-w-6xl px-6 m-auto shadow-3xl">
-        <nav className="flex justify-between py-6">
-          <div className="text-white text-2xl">
+        <nav className="relative flex-wrap sm:static flex justify-between py-6">
+          <div className="text-white text-2xl flex w-full sm:w-auto justify-between items-center">
             <Link title="israelalves" href="/">
               IsraelAlves
             </Link>
+
+            <button onClick={() => setNavOpen(!navOpen)}
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block sm:hidden outline-none focus:outline-none"
+            >
+              <List size={32} />
+            </button>
           </div>
 
-          <ul className="flex items-center gap-6">
-            <li>
-              <Link title="github" href="https://github.com/raelalves182" target="_blank">
-                <GithubLogo size={24} color="white" />
-              </Link>
-            </li>
-            <li>
-              <Link title="linkedin" href="https://www.linkedin.com/in/israel-alves-dev" target="_blank">
-                <LinkedinLogo size={24} weight="fill" color="white" />
-              </Link>
-            </li>
-            <li>
-              <Link title="twitter" href="https://twitter.com/IsraelAlves182" target="_blank">
-                <TwitterLogo size={24} weight="fill" color="white" />
-              </Link>
-            </li>
-            <li>
-              <Link title="instagram" href="https://www.instagram.com/_israelalves" target="_blank">
-                <InstagramLogo size={24} weight="fill" color="white" />
-              </Link>
-            </li>
-          </ul>
+          <div className={"sm:flex flex-grow items-center" + (navOpen ? " flex" : " hidden")} >
+            <ul className="flex flex-col sm:flex-row sm:ml-auto sm:gap-6" id="navbar-default">
+              <li>
+                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="github" href="https://github.com/raelalves182" target="_blank">
+                  <GithubLogo size={24} color="white" />
+                  <span className="ml-2 sm:hidden">Github</span>
+                </Link>
+              </li>
+              <li>
+                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="linkedin" href="https://www.linkedin.com/in/israel-alves-dev" target="_blank">
+                  <LinkedinLogo size={24} weight="fill" color="white" />
+                  <span className="ml-2 sm:hidden">Linkedin</span>
+                </Link>
+              </li>
+              <li>
+                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="twitter" href="https://twitter.com/IsraelAlves182" target="_blank">
+                  <TwitterLogo size={24} weight="fill" color="white" />
+                  <span className="ml-2 sm:hidden">Twitter</span>
+                </Link>
+              </li>
+              <li>
+                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="instagram" href="https://www.instagram.com/_israelalves" target="_blank">
+                  <InstagramLogo size={24} weight="fill" color="white" />
+                  <span className="ml-2 sm:hidden">Instagram</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </header>
