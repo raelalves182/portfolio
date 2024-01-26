bc2247b40c431747c08d8e81f8caad6d32e8b0e6
raelalves182 <ysrael.2009@hotmail.com>
2024-01-26 11:16:48 -0300

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,7 @@
 import { Key } from "react";
+import { Card, CardContent } from "@/components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+
 export default async function Users() {
   const res = await fetch('https://api.github.com/users/raelalves182/repos',
     { next: { revalidate: 3600 },
@@ -9,18 +12,33 @@ export default async function Users() {
   }
 
   const repos = await res.json();
-  console.log(repos)
 
   return (
     <section className="bg-hero-pattern bg-no-repeat bg-cover bg-center h-screen">
-      <div className="container max-w-6xl px-6 pt-10 m-auto h-full flex justify-center">
-        <ul className="flex flex-col items-center">
-          {repos.map((el: { id: Key | undefined; homepage: string }) => (
-            <li key={el.id} className="text-white">
-              <a href={el.homepage} target="_blank" rel="noreferrer">{el.homepage}</a>
-            </li>
-          ))}
-        </ul>
+      <div className="container max-w-6xl px-6 m-auto h-full flex justify-center items-center">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          orientation="vertical"
+          className="w-full max-w-xs"
+        >
+          <CarouselContent className="-mt-1 h-[200px]">
+            {repos.map((el: { id: Key | undefined; homepage: string }) => (
+              <CarouselItem key={el.id} className="pt-1 md:basis-1/2">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-6">
+                      <a href={el.homepage} target="_blank" rel="noreferrer">{el.homepage}</a>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </section>
   )
