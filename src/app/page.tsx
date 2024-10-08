4e1905b19864ae94c3700512fba2637b701de81e
raelalves182 <ysrael.2009@hotmail.com>
2024-10-07 21:26:26 -0300

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main>
         <section className="bg-hero-pattern bg-no-repeat bg-cover bg-center h-screen">
           <div className="container max-w-6xl px-6 m-auto h-full flex justify-center">
-            <div className="max-w-5xl m-auto p-6 sm:p-16 rounded-[32px] bg-black bg-opacity-75 text-center">
+            <div className="max-w-5xl m-auto p-6 sm:p-16 rounded-[32px] bg-black bg-opacity-75 text-center mt-28">
               <div className="inline-flex overflow-hidden w-[200px] h-[200px] border-2 border-teal-500 rounded-full mb-4 cursor-pointer">
                 <Image src="/profile.jpg" width={200} height={200} alt="Profile's Image" className="flex rounded-full m-auto object-cover h-52 object-bottom mb-6 transition-all ease-in hover:scale-125" />
               </div>
@@ -21,7 +21,7 @@ export default function Home() {
                 <Button>
                   <Link
                     href="mailto:ysrael.2009@hotmail.com"
-                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-sky-700 transition-all"
+                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-teal-500 transition-all"
                   >
                     Contact
                   </Link>
@@ -30,7 +30,7 @@ export default function Home() {
                 <Button>
                   <Link
                     href="/about"
-                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-sky-700 transition-all"
+                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-teal-500 transition-all"
                   >
                     About
                   </Link>
