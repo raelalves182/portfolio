cd5af4a7c75f549df446b82f4ca44e2b2b6a3c22
raelalves182 <ysrael.2009@hotmail.com>
2025-10-21 10:55:04 -0300

    refactor website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,42 @@ export default function Home() {
   return (
     <>
       <main>
-        <section className="bg-hero-pattern bg-no-repeat bg-cover bg-center h-screen">
-          <div className="container max-w-6xl px-6 m-auto h-full flex justify-center">
-            <div className="max-w-5xl m-auto p-6 sm:p-16 rounded-[32px] bg-black bg-opacity-75 text-center mt-28">
-              <div className="inline-flex overflow-hidden w-[200px] h-[200px] border-2 border-teal-500 rounded-full mb-4 cursor-pointer">
-                <Image src="/profile.jpg" width={200} height={200} alt="Profile's Image" className="flex rounded-full m-auto object-cover h-52 object-bottom mb-6 transition-all ease-in hover:scale-125" />
+        if (condition) {
+          <div>
+            <section className="mt-[50px]">
+              <div className='container'>
+                <div className='flex items-center justify-between gap-24'>
+                  <div>
+                    <span className='text-2xl -tracking-[2px]'>Hey, I&apos;m Israel</span>
+                    <h1 className='text-[90px] leading-[90px] font-bold'><span className='text-blue-600'>Front</span>end Developer</h1>
+                  </div>
+
+                  <div className='block border border-blue-600 p-6 rounded-full'>
+                    <Image
+                      src="/me.png"
+                      width={440}
+                      height={440}
+                      alt="Picture of the author"
+                      className='rounded-[240px] object-cover object-top h-[440px] sm:max-w-none'
+                    />
+                  </div>
+                </div>
               </div>
+            </section>
 
-              <h1 className="text-4xl text-[#DDDADC] font-bold sm:text-6xl">Israel <span className="text-teal-500">Alves</span></h1>
-
-              <p className="text-base sm:text-2xl text-[#DDDADC] my-2 sm:my-4">Front-end Developer</p>
-
-              <div className="flex items-center justify-center gap-4">
-                <Button>
-                  <Link
-                    href="mailto:ysrael.2009@hotmail.com"
-                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-teal-500 transition-all"
-                  >
-                    Contact
-                  </Link>
-                </Button>
-
-                <Button>
-                  <Link
-                    href="/about"
-                    className="block text-base sm:text-2xl text-[#DDDADC] border px-4 py-2 rounded-full hover:bg-teal-500 transition-all"
-                  >
-                    About
-                  </Link>
-                </Button>
+            <section className='mt-[50px]'>
+              <div className="container">
+                <div>
+                  <h2 className='text-[56px] leading-[67px] font-bold'>Projects<span className='text-blue-600'>.</span></h2>
+                </div>
               </div>
-            </div>
+            </section>
           </div>
-        </section>
+        } else {
+          <div>
+            <h1>Condition is false</h1>
+          </div>
+        }
       </main>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,22 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { GithubLogo, InstagramLogo, LinkedinLogo, List, XLogo } from '@phosphor-icons/react';
+import { GithubLogo, InstagramLogo, LinkedinLogo, List } from '@phosphor-icons/react';
 
 const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <header className="flex items-center h-auto w-full bg-[#090A0B]">
-      <div className="container max-w-6xl px-6 m-auto shadow-3xl">
+    <header className="flex items-center h-auto w-full">
+      <div className="container">
         <nav className="relative flex-wrap sm:static flex justify-between py-6">
-          <div className="text-white text-2xl flex w-full sm:w-auto justify-between items-center">
+          <div className="text-neutral-700 text-base font-bold flex w-full sm:w-auto justify-between items-center">
             <Link title="israelalves" href="/">
               IsraelAlves
             </Link>
 
             <button onClick={() => setNavOpen(!navOpen)}
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded-sm bg-transparent block sm:hidden outline-hidden focus:outline-hidden"
+              className="text-neutral-700 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded-sm bg-transparent block sm:hidden outline-hidden focus:outline-hidden"
             >
               <List size={32} />
             </button>
@@ -26,26 +26,20 @@ const Header = () => {
           <div className={"sm:flex grow items-center" + (navOpen ? " flex" : " hidden")} >
             <ul className="flex flex-col sm:flex-row sm:ml-auto sm:gap-6" id="navbar-default">
               <li>
-                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="github" href="https://github.com/raelalves182" target="_blank">
-                  <GithubLogo size={24} color="white" />
+                <Link className="py-2 px-3 flex items-center text-neutral-700 text-xs uppercase hover:opacity-75" title="github" href="https://github.com/raelalves182" target="_blank">
+                  <GithubLogo size={24} color="black" />
                   <span className="ml-2 sm:hidden">Github</span>
                 </Link>
               </li>
               <li>
-                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="linkedin" href="https://www.linkedin.com/in/israel-alves-dev" target="_blank">
-                  <LinkedinLogo size={24} color="white" />
+                <Link className="py-2 px-3 flex items-center text-neutral-700 text-xs uppercase hover:opacity-75" title="linkedin" href="https://www.linkedin.com/in/israel-alves-dev" target="_blank">
+                  <LinkedinLogo size={24} color="black" />
                   <span className="ml-2 sm:hidden">Linkedin</span>
                 </Link>
               </li>
-              {/* <li>
-                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="twitter" href="https://twitter.com/IsraelAlves182" target="_blank">
-                  <XLogo size={24} color="white" />
-                  <span className="ml-2 sm:hidden">Twitter</span>
-                </Link>
-              </li> */}
               <li>
-                <Link className="py-2 px-3 flex items-center text-white text-xs uppercase hover:opacity-75" title="instagram" href="https://www.instagram.com/_israelalves" target="_blank">
-                  <InstagramLogo size={24} color="white" />
+                <Link className="py-2 px-3 flex items-center text-neutral-700 text-xs uppercase hover:opacity-75" title="instagram" href="https://www.instagram.com/_israelalves" target="_blank">
+                  <InstagramLogo size={24} color="black" />
                   <span className="ml-2 sm:hidden">Instagram</span>
                 </Link>
               </li>
