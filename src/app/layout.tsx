f731b4411cf608ebd9d4c6cd567fe9186e3f0c00
raelalves182 <ysrael.2009@hotmail.com>
2025-10-21 23:31:18 -0300

    update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import { Josefin_Sans } from 'next/font/google';
 import Header from '@/components/Header';
-import './styles/globlas.css';
+import { useEffect } from 'react';
 import Head from './head';
+import Aos from 'aos';
+// CSS
+import './styles/globlas.css';
+import 'aos/dist/aos.css';
 
 const josefin_sans = Josefin_Sans({
   subsets: ["latin"],
@@ -13,6 +19,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <html lang="pt-br" className={josefin_sans.className}>
       <Head />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Button from '@/components/Button';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,30 +6,29 @@ export default function Home() {
       <main>
         <section className="mt-[50px]">
           <div className='container'>
-            <div className='flex items-center justify-between gap-24'>
-              <div>
+            <div className='flex items-center justify-between gap-2 max-lg:flex-col max-lg:gap-8'>
+              <div data-aos="fade-right">
                 <span className='text-2xl -tracking-[2px]'>Hey, I&apos;m Israel</span>
-                <h1 className='text-[90px] leading-[90px] font-bold'><span className='text-blue-600'>Front</span>end Developer</h1>
+                <h1 className='text-[90px] leading-[90px] font-bold max-md:text-6xl max-md:leading-none'><span className='text-blue-600'>Front</span>end Developer</h1>
               </div>
 
-              <div className='block border border-blue-600 p-6 rounded-full'>
+              <div className='block border border-blue-600 p-6 rounded-full w-full h-[440px] max-w-[440px] max-sm:h-auto' data-aos="fade-left">
                 <Image
                   src="/me.png"
                   width={440}
                   height={440}
                   alt="Picture of the author"
-                  className='rounded-[240px] object-cover object-top h-[440px] sm:max-w-none'
+                  className='rounded-[240px] object-cover object-top w-full h-full'
+                  loading="eager"
                 />
               </div>
             </div>
           </div>
         </section>
 
-        <section className='mt-[50px]'>
+        <section className='mt-[175px] max-lg:mt-14'>
           <div className="container">
-            <div>
-              <h2 className='text-[56px] leading-[67px] font-bold'>Projects<span className='text-blue-600'>.</span></h2>
-            </div>
+            <h2 className='text-[56px] leading-[67px] font-bold'>Projects<span className='text-blue-600'>.</span></h2>
           </div>
         </section>
       </main>
